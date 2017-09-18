e67f5a565b3d665f1b3bbc19a4a08dbd6335ec63
Juan Oliver <joliver@juanoliver.net>
2017-09-18 16:54:07 -0500

    Se aplicó estilo en linea a los links de CreativeCommon; se cambió de archivo la propiedad que aplicaba a los copy del footer, se cambió el color de hover de los link; se aplicaron configuraciones a los hero-section de cada página; se aumentó el tiempo de animación de los a del menú, se cambió el color del link en el hover, se agregó un espacio a la izquierda de los iconfonts igual al espacio de la derecha, se eliminaron los iconos del menú por incompatibilidad con los submenú de Foundation; se agregó el archivo update.scss para configurar la presentación de la última fecha de actualización de los documentos; se agregaron JS de Office 365 y de Google y ZOHO; se crearon las páginas de capacitaciones, contacto, nosotros, infraestructura, segurinfo, website y los documentos términos y polñíticas; se actualizaron las rutas de los documentos en el footer; se creó el archivo humans.txt; se metió todo el contenido del index en una sección; se creo publicidad con google Adsense;

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,2 +1,28 @@
 $(document).foundation();
 
+//Office 365
+var ccs_cc_args = ccs_cc_args || [];
+(function () {
+    var o = ccs_cc_args; o.push(['_SKey', 'bacdc6d3']); o.push(['_ZoneId', 'emdedESLoffice365']);
+    var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true;
+    sc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.cnetcontent.com/jsc/h.js';
+    var n = document.getElementsByTagName('script')[0]; n.parentNode.insertBefore(sc, n);
+})();
+
+//Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-106424322-1', 'auto');
+ga('send', 'pageview');
+
+// Google AdSense
+(adsbygoogle = window.adsbygoogle || []).push({});
+
+//ZOHO CRM
+var $zoho= $zoho || {salesiq:{values:{},ready:function(){
+    $zoho.salesiq.floatbutton.visible('hide');}}};
+var d=document;s=d.createElement('script');s.type='text/javascript';
+s.defer=true;s.src='https://salesiq.zoho.com/juanoliver/float.ls?embedname=juanoliver';t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);
