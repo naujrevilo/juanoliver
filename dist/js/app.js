3be756059bbb827ce15f6aec57f2e86c1c3e3aa7
Juan Oliver <joliver@juanoliver.net>
2017-09-21 19:36:53 -0500

    Se agregaron los scripts de Google Analytics. Se agregaron links al index para los servicios en la tienda.

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,13 +10,13 @@ var ccs_cc_args = ccs_cc_args || [];
 })();
 
 //Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)};
+gtag('js', new Date());
 
-ga('create', 'UA-106424322-1', 'auto');
-ga('send', 'pageview');
+gtag('config', 'UA-26191186-1');
+
 
 // Google AdSense
 (adsbygoogle = window.adsbygoogle || []).push({});
